8c888698a3428a8ef60d3527d7fc0bc7c0804a97
Simon Hales <simon.h@iconinteractive.com>
2020-12-28 22:07:21 +1000

    added sentry for logging

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN ?? "",
+    autoSessionTracking: true,
+    integrations: [
+        new Integrations.BrowserTracing(),
+    ],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+});
 
 window.oncontextmenu = function() { return false; }
 
