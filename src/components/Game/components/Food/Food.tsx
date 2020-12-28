99dbb9d5097567930a15c1f40fd47da251236432
Simon Hales <simon.h@iconinteractive.com>
2020-12-28 13:05:28 +1000

    disable scale animation

--- src/components/Game/components/Food/Food.tsx
+++ src/components/Game/components/Food/Food.tsx
@@ -13,20 +13,20 @@ const Food: React.FC<{
 }> = ({data}) => {
     const {id, position, food} = data
 
-    const { spring } = useSpring({
-        spring: food / 50,
-        config: { mass: 5, tension: 400, friction: 50, precision: 0.0001 },
-    })
-
-    const scale = spring.to([0, 1], [0.1, 1])
+    // const { spring } = useSpring({
+    //     spring: food / 50,
+    //     config: { mass: 5, tension: 400, friction: 50, precision: 0.0001 },
+    // })
+    //
+    // const scale = spring.to([0, 1], [0.1, 1])
 
     return (
         <group position={[position[0], position[1], 0]}>
-            <a.group scale-x={scale} scale-y={scale}>
+            {/*<a.group scale-x={scale} scale-y={scale}>*/}
                 <Cylinder args={[radius, radius, 0.1, 20]} rotation={[radians(90), 0, 0]} receiveShadow castShadow>
                     <meshBasicMaterial color={color} transparent opacity={1} />
                 </Cylinder>
-            </a.group>
+            {/*</a.group>*/}
         </group>
     )
 }
